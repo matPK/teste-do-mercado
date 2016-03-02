67e525009192b03a0aee0c5f6aa428ac39636d54
Matheus Adorni Dardenne <mat.dardenne@gmail.com>
2016-03-02 14:20:55 -0300

    Revisão
    Revisão do espaçamento, da identação e remoção do código "lixo" de teste.

--- php/submit.php
+++ php/submit.php
@@ -3,19 +3,19 @@
 	include "Dao.php";
 	include "Operacao.php";
 	include "Conexao.php";
-
-
+	
 	$nome_mer	= $_POST['nome_mer'];
 	$cod_mer	= $_POST['cod_mer'];
 	$tipo_mer 	= $_POST['tipo_mer'];
 	$valor		= $_POST['valor'];
 	$qnt 		= $_POST['qnt'];
-	$tipo_neg	= 1;
+	$tipo_neg	= 1;//Compra é 1, venda é 0.
 	if(isset($_POST['venda']) && $_POST['venda'] == '0'){
 		$tipo_neg = 0;
 	}
 	
 	if(empty($nome_mer) || empty($cod_mer) || empty($tipo_mer) || empty($valor)){
+		header('Location: ../index.html');
 		echo "Há um erro no preenchimento dos campos!";
 	}else{
 		$dao = DaoOperacoes::getInstance();
@@ -28,8 +28,8 @@
 		$pojoOp->setTipo_neg($tipo_neg);
 		if($dao->Inserir($pojoOp)){
 			echo "Inserido com sucesso!";
+		}else{
+			echo "Não foi possível concluir a operação!"
 		}
 	}
-	
-	
 ?>
