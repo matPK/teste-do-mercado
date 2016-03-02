21a61e539c5bfcb3bcd0318cc8bf554ecc821c38
Matheus Adorni Dardenne <mat.dardenne@gmail.com>
2016-03-02 15:41:52 -0300

    Revisão final.
    Revisão final. Código funcional.

--- php/retrieve.php
+++ php/retrieve.php
@@ -10,10 +10,16 @@
 	$c = 1;
 	if(isset($lista)){
 		foreach($lista as $i){
-			echo "Código da operação ".$c.": ".$i->getCod_mer();
+			echo "Codigo da operacao ".$c.": ".$i->getCod_mer();
+			if ($i->getTipo_neg() == 1){
+				echo ", tipo da operacao: Compra.";
+			}else{
+				echo ", tipo da operacao: Venda.";
+			}
+			echo "<br>";
 			$c++;
 		}
 	}else{
-		echo "Não foi possível concluir a operação";
+		echo "Nao foi possivel concluir a operacao";
 	}
 ?>
