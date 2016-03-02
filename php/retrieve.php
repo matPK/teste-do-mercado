8fdd3f160ca22bfe815961709f1dcbebb11e5760
Matheus Adorni Dardenne <mat.dardenne@gmail.com>
2016-03-02 14:22:29 -0300

    Adição do retrieve
    O retrieve serve para buscar dados no banco e exibi-los na tela.

---
+++ php/retrieve.php
@@ -0,0 +1,19 @@
+<?php
+
+	include "Dao.php";
+	include "Operacao.php";
+	include "Conexao.php";
+
+
+	$dao = DaoOperacoes::getInstance();
+	$lista = $dao->Buscar();
+	$c = 1;
+	if(isset($lista)){
+		foreach($lista as $i){
+			echo "Código da operação ".$c.": ".$i->getCod_mer;
+			$c++;
+		}
+	}else{
+		echo "Não foi possível concluir a operação";
+	}
+?>
